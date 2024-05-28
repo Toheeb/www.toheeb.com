6a5165b1612cdd00bf1409c7564950cf27308799
Toheeb Ogunbiyi <toogunbiyi@gmail.com>
2024-05-28 17:53:07 +0100

    include base stylesheet via a linked node_module

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (config) {
   config.addPassthroughCopy("src/netlify.toml");
   config.addPassthroughCopy({ "src/en/_images/": "/en/" });
   config.addPassthroughCopy({ "src/en/_assets/": "/en/" });
+  config.addPassthroughCopy({ "node_modules/@toheeb/prose/index.css": "/en/base.css" });
 
   return {
     htmlTemplateEngine: "njk",
