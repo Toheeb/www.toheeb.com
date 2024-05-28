a374af76c65a42211e7e1fb24bdd308502072fb5
Toheeb Ogunbiyi <toogunbiyi@gmail.com>
2024-05-28 17:33:04 +0100

    site navigation

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (config) {
   config.addPassthroughCopy("src/netlify.toml");
   config.addPassthroughCopy({ "src/en/_images/": "/en/" });
+  config.addPassthroughCopy({ "src/en/_assets/": "/en/" });
 
   return {
     htmlTemplateEngine: "njk",
