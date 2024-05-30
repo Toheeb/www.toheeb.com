8a9086072f9300061e1286e546a6eded0e6b0e9f
Toheeb Ogunbiyi <toogunbiyi@gmail.com>
2024-05-30 02:37:51 +0100

    prepare for the chapters of HTML by Contents

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,70 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation")
+
+
 module.exports = function (config) {
   config.addPassthroughCopy("src/netlify.toml");
   config.addPassthroughCopy({ "src/en/_images/": "/en/" });
   config.addPassthroughCopy({ "src/en/_assets/": "/en/" });
   config.addPassthroughCopy({ "node_modules/@toheeb/prose/index.css": "/en/base.css" });
+
+  config.addPlugin(eleventyNavigationPlugin);
+
+	config.addCollection("hc-parts-and-chapters", api => {
+
+		const hcPreface = api.getFilteredByTags('hc-preface').sort(ascendingOrder)
+		const hcPreamble = api.getFilteredByTags('hc-preamble').sort(ascendingOrder)
+		const hcPrimal = api.getFilteredByTags('hc-primal').sort(ascendingOrder)
+		const hcPhrasing = api.getFilteredByTags('hc-phrasing').sort(ascendingOrder)
+		const hcProtocol = api.getFilteredByTags('hc-protocol').sort(ascendingOrder)
+
+    const hcOutline = [];
+    
+    if (hcPreface.length > 0) {
+      hcOutline.push({
+        name: "Content Preface",
+        chapters: hcPreface,
+        cummulativeChapterLength: 1
+      })
+    }
+    
+    if (hcPreamble.length > 0) {
+      hcOutline.push({
+        name: "Preamble Contents",
+        chapters: hcPreamble,
+        cummulativeChapterLength: 1 + hcPreface.length
+      })
+    }
+    
+    if (hcPrimal.length > 0) {
+      hcOutline.push({
+        name: "Primal Contents",
+        chapters: hcPrimal,
+        cummulativeChapterLength: 1 + hcPreface.length + hcPreamble.length
+      })
+    }
+    
+    if (hcPhrasing.length > 0) {
+      hcOutline.push({
+        name: "Phrasing Contents",
+        chapters: hcPhrasing,
+        cummulativeChapterLength: 1 + hcPreface.length + hcPreamble.length + hcPrimal.length
+      })
+    }
+    
+    if (hcProtocol.length > 0) {
+      hcOutline.push({
+        name: "Content Protocols",
+        chapters: hcProtocol,
+        cummulativeChapterLength: 1 + hcPreface.length + hcPreamble.length + hcPrimal.length + hcPhrasing.length
+      })
+    }
+
+		return hcOutline;
+	})
+
+  config.addCollection("hc-chapters", api => {
+    return api.getFilteredByTags("hc").sort(ascendingOrder);
+  })
 
   return {
     htmlTemplateEngine: "njk",
@@ -14,3 +76,7 @@ module.exports = function (config) {
     },
   };
 };
+
+function ascendingOrder(a, b) {
+	return Number(a.data.hcChapter) > Number(b.data.hcChapter) ? 1 : -1
+}
