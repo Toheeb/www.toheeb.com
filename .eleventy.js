195ccb7a0cd40b834ccea5e120220c405a07f209
Toheeb Ogunbiyi <toogunbiyi@gmail.com>
2024-11-01 09:03:19 +0100

    move _redirects to publish directory

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation")
 
 
 module.exports = function (config) {
-  config.addPassthroughCopy("src/netlify.toml");
+  config.addPassthroughCopy("src/_redirects");
   config.addPassthroughCopy({ "src/en/_images/": "/en/" });
   config.addPassthroughCopy({ "src/en/_assets/": "/en/" });
   config.addPassthroughCopy({ "node_modules/@toheeb/prose/index.css": "/en/base.css" });
